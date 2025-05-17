c11f8ce210ee568ca1edded70e02e73f823d732d
Henry Lee <henryleeworld@gmail.com>
2025-05-18 00:09:49 +0800

    feat:更新最新資料

--- lang/zh_TW/admin.php
+++ lang/zh_TW/admin.php
@@ -4,7 +4,7 @@ return [
     'profile' => [
         'title'   => '我的個人資料',
         'content' => [
-            'information'            => '當您將帳戶設置為個人時，最初會顯示「個人資料」頁面。',
+            'information'            => '當您將帳號設置為個人時，最初會顯示「個人資料」頁面。',
             'show_hide_help_section' => '顯示／隱藏幫助部分',
             'successfully_saved'     => '儲存成功。',
         ],
